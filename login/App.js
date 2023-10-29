2db31dc3e8a4916d04f08304d71d10c33b8526d4
Rafael Alves Freitas da Silva <46302153+rafaelafsinfo@users.noreply.github.com>
2023-10-29 13:40:50 -0300

    update

--- login/App.js
+++ login/App.js
@@ -5,12 +5,6 @@ const Route_Alunos = require("./Router/Route_Alunos")
 //const Route_Professor = require("./Router/Route_Professor")
 
 const app = express()
-const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'colegiosunivap'
-})
 
 const banco = mysql.createPool({
 connectionLimit: 128,
@@ -24,16 +18,13 @@ app.use(express.json())
 app.use('/view', express.static(__dirname + '/view'))
 
 Route_Alunos(app,banco)
-//Route_Professor(app,banco)
+Route_Professor(app,banco)
 
 
 app.listen(3000, function () {
   console.log('Servidor Ativo na porta: ' + 3000 + '!')
 });
-con.connect(function(err){
-  if (err) throw err;
-  console.log("Connected!")
-})
+
 
 
 module.exports = app
---
+++ login/Router/Route_Professor.js
@@ -0,0 +1,54 @@
+module.exports = function(app,banco){
+
+    const JwtToken = require('../Model/JwtToken')
+    const Alunos = require("../Model/Alunos")
+    app.post('/loginaluno',(request,response) =>{
+        console.log("rota: POST: /login")
+            const matricula = request.body.matricula
+            const senha = request.body.senha
+    
+            const Aluno = new Alunos(banco)
+            Aluno.setmatricula(matricula)
+            Aluno.setSenha(senha)
+    
+            Aluno.login().then((respostaLogin) => {
+            if (respostaLogin.status == true) { 
+                let usuario = {
+                matricula: respostaLogin.matricula,
+                nome: respostaLogin.nome
+                }
+                const jwt = new JwtToken()
+                const novoToken = jwt.gerarToken(usuario)
+
+                const resposta = {
+                status: true,
+                msg: "Login efetuado com sucesso",
+                token: novoToken,
+                Aluno: usuario
+                }
+    
+                response.status(201).send(resposta)
+            } else {
+                const resposta = {
+                status: false,
+                msg: "Usuário não logado",
+                codigo: 401,
+                }
+                response.send(resposta, 404)
+            }
+    
+            }).catch((erro) => {
+            const resposta = {
+                status: false,
+                msg: 'erro ao executar',
+                codigo: '005',
+                dados: erro,
+            }
+    
+    
+    
+            response.status(201).send(erro);
+            });
+    })
+
+}
